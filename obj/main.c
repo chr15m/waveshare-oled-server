d760f0cbda6a725ddade9a4efbb57b060e0edf5f
Chris McCormick <chris@mccormick.cx>
2018-07-23 18:14:12 +0800

    Initial UDP socket code.

--- obj/main.c
+++ obj/main.c
@@ -4,58 +4,65 @@
 #include "OLED_Driver.h"
 #include "OLED_GUI.h"
 #include "DEV_Config.h"
-#include <time.h>
 
-char value[10] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
-int main(void)
-{
-	time_t now;
-    struct tm *timenow;
-	//1.System Initialization
-	if(System_Init())
-		exit(0);
-	
-	//2.show
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+#define BUFLEN 4096
+#define PORT 3323
+
+void die(char *s) {
+    perror(s);
+    exit(1);
+}
+
+int main(void) {
+	struct sockaddr_in si_me, si_other;
+	int s, slen = sizeof(si_other), recv_len;
+	char buf[BUFLEN];
+
+	// 1.System Initialization
+	if (System_Init()) exit(0);
+
+	// create UDP socket
+	if ((s = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)	{
+		die("socket");
+	}
+
+	// zero out the structure
+	memset((char *) &si_me, 0, sizeof(si_me));
+
+	si_me.sin_family = AF_INET;
+	si_me.sin_port = htons(PORT);
+	si_me.sin_addr.s_addr = htonl(INADDR_ANY);
+
+	// bind socket to port
+	if( bind(s , (struct sockaddr*)&si_me, sizeof(si_me) ) == -1) {
+		die("bind");
+	}
+
+	// 2.show
 	printf("**********Init OLED**********\r\n");
 	OLED_SCAN_DIR OLED_ScanDir = SCAN_DIR_DFT;//SCAN_DIR_DFT = D2U_L2R
-	OLED_Init(OLED_ScanDir );	
-	
-	printf("OLED Show \r\n");
-	GUI_Show();
-	
-	while(1){
-		time(&now);
-		timenow = localtime(&now);
-	
-		//GUI_Disbitmap(0, 2, Signal816, 16, 8);
-		//GUI_Disbitmap(24, 2, Bluetooth88, 8, 8);
-		//GUI_Disbitmap(40, 2, Msg816, 16, 8);
-		//GUI_Disbitmap(64, 2, GPRS88, 8, 8);
-		//GUI_Disbitmap(90, 2, Alarm88, 8, 8);
-		//GUI_Disbitmap(112, 2, Bat816, 16, 8);
-		
-		GUI_DisString_EN(0, 0, "hello", &Font12, FONT_BACKGROUND, WHITE); 
-		//GUI_DisString_EN(0, 52, "MUSIC", &Font12, FONT_BACKGROUND, WHITE); 
-		//GUI_DisString_EN(52, 52, "MENU", &Font12, FONT_BACKGROUND, WHITE); 
-		//GUI_DisString_EN(98, 52, "PHONE", &Font12, FONT_BACKGROUND, WHITE);
+	OLED_Init(OLED_ScanDir);
 
-		//GUI_DisChar(0, 0, "Hello", &Font24, FONT_BACKGROUND, WHITE);
-		//GUI_DisChar(0, 16, value[timenow->tm_hour / 10], &Font24, FONT_BACKGROUND, WHITE);
-		//GUI_DisChar(16, 16, value[timenow->tm_hour % 10], &Font24, FONT_BACKGROUND, WHITE);
-		//GUI_DisChar(32, 16, ':', &Font24, FONT_BACKGROUND, WHITE);
-		//GUI_DisChar(48, 16, value[timenow->tm_min / 10],  &Font24, FONT_BACKGROUND, WHITE);
-		//GUI_DisChar(64, 16, value[timenow->tm_min % 10],  &Font24, FONT_BACKGROUND, WHITE);
-		//GUI_DisChar(80, 16, ':',  &Font24, FONT_BACKGROUND, WHITE);
-		//GUI_DisChar(96, 16, value[timenow->tm_sec / 10],  &Font24, FONT_BACKGROUND, WHITE);
-		//GUI_DisChar(112, 16, value[timenow->tm_sec % 10],  &Font24, FONT_BACKGROUND, WHITE);
-		
-		OLED_Display();		
+	printf("OLED Show \r\n");
+	//GUI_Show();
+	GUI_DisString_EN(0, 0, "hello", &Font12, FONT_BACKGROUND, WHITE); 
+
+	while(1) {
 		OLED_Clear(0x00);
+		memset(buf, 0, BUFLEN);
+		// block on network receive
+		if ((recv_len = recvfrom(s, buf, BUFLEN, 0, (struct sockaddr *) &si_other, &slen)) == -1) {
+		    die("recvfrom()");
+		}
+        	GUI_DisString_EN(0, 0, buf, &Font12, FONT_BACKGROUND, WHITE); 
+		OLED_Display();		
 	}
-	
-	//3.System Exit
+
+	// 3.System Exit
 	System_Exit();
 	return 0;
-	
 }
 
